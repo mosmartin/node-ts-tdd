4a4b05ff6623e40235928ed9ebc09f8ea3ae088a
Martin Macharia <mosmartin5@gmail.com>
2020-11-24 14:07:53 +0100

    Add a parseUrl method
    Create a parseUrl method and it's unit tests.

--- src/tests/Utils.test.ts
+++ src/tests/Utils.test.ts
@@ -1,8 +1,41 @@
 import { Utils } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  it("should pass", () => {
-    const text = Utils.toUpperCase("mos martin");
-    expect(text).toBe("MOS MARTIN");
+  it("should parse a simple url", () => {
+    const parsedUrl = Utils.parseUrl("http://localhost:3000/login");
+
+    expect(parsedUrl.href).toBe("http://localhost:3000/login");
+    expect(parsedUrl.port).toBe("3000");
+    expect(parsedUrl.protocol).toBe("http:");
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  it("should pass url with a query", () => {
+    const parsedUrl = Utils.parseUrl(
+      "http://localhost:3000/login?user=user&password=pass"
+    );
+
+    const expectedQuery = {
+      user: "user",
+      password: "pass",
+    };
+
+    expect(parsedUrl.query).toEqual(expectedQuery);
+    expect(expectedQuery).toBe(expectedQuery);
+  });
+
+  it("should test for invalid url using an arrow function", () => {
+    expect(() => {
+      Utils.parseUrl("");
+    }).toThrowError("Empty URL!");
+  });
+
+  it.only("should test for invalid url using a try catch block", () => {
+    try {
+      Utils.parseUrl("");
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty("message", "Empty URL!");
+    }
   });
 });
